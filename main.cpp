74c3ddac3931016aa9443c6f2c378aa6f7988163
Volodymyr T. Sukhoteplyy <V.Sukhoteplyy@ukr.net>
2015-08-05 23:20:30 +0300

    After trobbles

--- main.cpp
+++ main.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
-   int i=17;
-   int j=29;
+int i=17;
+int j=29;
 
-   int* p1 =&i;
-   cout << p1 << endl;
-   cout << *p1 << endl;
-
-   *p1 = 103;
-cout << i << endl;
+int* p1 = &i;
+cout << p1 << endl;
+cout << *p1 << endl;
 
 	return 0;
 }

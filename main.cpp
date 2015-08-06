8f5647a9aa0b13a00c8e4a44727da7a74f2b8a19
Volodymyr T. Sukhoteplyy <V.Sukhoteplyy@ukr.net>
2015-08-06 21:59:46 +0300

    After restyling

--- main.cpp
+++ main.cpp
@@ -1,18 +1,18 @@
-//Place to put comments
+// Place to put comments
 #include <iostream>
 using namespace std;
 
 int main()
 {
-int i=17;
-int j=29; // To use later
+    int i = 17;
+    int j = 29; // To use later
 
-int* p1 = &i;
-int* p2 = &j;
-cout << p1 << endl;
-cout << *p1 << endl;
+    int* p1 = &i;
+    int* p2 = &j;
+    cout << p1 << endl;
+    cout << *p1 << endl;
 
-cout << p2 << endl;
-cout << *p2 << endl;
-	return 0;
+    cout << p2 << endl;
+    cout << *p2 << endl;
+    return 0;
 }
